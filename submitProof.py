1d495ad44de071ed4735ed6a1df38e69b44d8192
rfetyan94 <rfetyan@seas.upenn.edu>
2025-07-15 16:12:07 -0600

    Update submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -145,7 +145,6 @@ def send_signed_msg(proof, random_leaf):
     })
 
     signed_tx = w3.eth.account.sign_transaction(tx, private_key=acct.key)
-
     tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
 
     return tx_hash
